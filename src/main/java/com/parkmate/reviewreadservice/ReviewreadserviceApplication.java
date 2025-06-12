90246cfd80c085b1f8e8a14b59f0732dae0d0a9f
bomikim <kbm8527@gmail.com>
2025-06-12 14:12:13 +0900

    Initial commit

---
+++ src/main/java/com/parkmate/reviewreadservice/ReviewreadserviceApplication.java
@@ -0,0 +1,13 @@
+package com.parkmate.reviewreadservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewreadserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewreadserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/parkmate/reviewreadservice/ReviewreadserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parkmate.reviewreadservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewreadserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
